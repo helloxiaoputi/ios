64c3323707ecfa47e3b46f49c54e8e86c1476072
helloxiaoputi <xiao_pu_ti@163.com>
2018-09-08 10:16:18 +0800

    qqq

--- hello/hello/ViewController.swift
+++ hello/hello/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  hello
 //
-//  Created by 林斌 on 2018/9/4.
+//  Created by 林斌 on 2018/9/5.
 //  Copyright © 2018年 林少. All rights reserved.
 //
 
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
